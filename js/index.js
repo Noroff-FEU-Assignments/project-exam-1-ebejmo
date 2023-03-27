28471d8d8210340b63dc47238fcc51b27c865be2
ebejmo <94139531+ebejmo@users.noreply.github.com>
2023-03-27 18:55:03 +0200

    footer and header + home hero

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
+const heroContainer = document.querySelector(".hero");
 const blogContainer = document.querySelector(".blog");
 const baseUrl =
   "https://e-bjm.no/threeput/wp-json/wp/v2/posts?_embed&per_page=12";
+const homeUrl = "https://e-bjm.no/threeput/wp-json/wp/v2/pages/30?_embed";
 
 async function getPosts() {
   try {
     const response = await fetch(baseUrl);
     const results = await response.json();
 
-    console.log(results);
+    // console.log(results);
     blogContainer.innerHTML = "";
 
     results.forEach(function (blogs) {
-      console.log(blogs.content.rendered);
+      //   console.log(blogs._embedded["wp:featuredmedia"][0].source_url);
       blogContainer.innerHTML += `
         <div>
-        <div>
-        ${blogs.content.rendered}
-        </div>
+            <div>
+                <h3>${blogs.title.rendered}</h3>
+                <img src=${blogs._embedded["wp:featuredmedia"][0].source_url}>
+            </div>
         </div>`;
     });
   } catch (error) {
@@ -24,4 +27,27 @@ async function getPosts() {
   }
 }
 
-getPosts();
+// getPosts();
+
+async function getHomeContent() {
+  try {
+    const response = await fetch(homeUrl);
+    const data = await response.json();
+
+    console.log(data);
+
+    heroContainer.innerHTML += `
+    <div class="hero-img" style="background-image: url(${data._embedded["wp:featuredmedia"][0].source_url}">
+        <div class="hero-content">
+            <h1>${data.title.rendered}</h1>
+            <h2>${data.content.rendered}</h2>
+        </div>
+        
+    </div>
+`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getHomeContent();
